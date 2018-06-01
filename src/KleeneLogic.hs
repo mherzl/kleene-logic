cbe191e8fdffa029a5aae781808990fa9707748f
matthew <matthew.herzl@gmail.com>
2018-06-01 23:18:15 +0000

    created not, and, or

---
+++ src/KleeneLogic.hs
@@ -0,0 +1,27 @@
+module KleeneLogic where
+
+import Prelude (Eq)
+
+data Kleene = False
+            | Unknown
+            | True
+            deriving Eq
+
+not :: Kleene -> Kleene
+not False = True
+not True = False
+not Unknown = Unknown
+
+and :: Kleene -> Kleene -> Kleene
+and False _ = False
+and _ False = False
+and True True = True
+and _ _ = Unknown
+
+or :: Kleene -> Kleene -> Kleene
+or True _ = True
+or _ True = True
+or False False = False
+or _ _ = True
+
+
